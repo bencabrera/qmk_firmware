f818cafbddf2a2e00677c20c9b3e988d01b92c88
Benjamin Cabrera <benny@bcabrera.de>
2023-05-15 11:28:54 +0200

    Added CTRL key on right thumb, some more stuff

--- keyboards/splitkb/aurora/corne/keymaps/bencabrera/combos.c
+++ keyboards/splitkb/aurora/corne/keymaps/bencabrera/combos.c
@@ -5,29 +5,32 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 
 enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
+    ESC_AND_SAVE = SAFE_RANGE,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case QMKBEST:
+    case ESC_AND_SAVE:
         if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
+            // when keycode ESC_AND_SAVE is pressed
             SEND_STRING(SS_TAP(X_ESC));
             SEND_STRING(":w");
             SEND_STRING(SS_TAP(X_ENT));
         } else {
-            // when keycode QMKBEST is released
+            // when keycode ESC_AND_SAVE is released
         }
         break;
     }
     return true;
 };
 
-const uint16_t PROGMEM test_combo1[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM test_combo2[] = {KC_S, KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM esc_combo[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM esc_and_hold_combo[] = {KC_S, KC_D, KC_F, COMBO_END};
+/*const uint16_t PROGMEM tab_combo[] = {KC_Q, KC_W, COMBO_END};*/
+
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(test_combo1, KC_ESC),
-    COMBO(test_combo2, QMKBEST),
+    COMBO(esc_combo, KC_ESC),
+    COMBO(esc_and_hold_combo, ESC_AND_SAVE),
+    /*COMBO(tab_combo, KC_TAB),*/
     /*COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!*/
 };
